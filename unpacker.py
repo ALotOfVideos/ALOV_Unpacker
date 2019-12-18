5f1d33d0d15e62df94207582910849c04d39bd9c
AlphaAtlas <46462706+AlphaAtlas@users.noreply.github.com>
2019-12-18 17:59:13 -0500

    update unpacker.py

--- unpacker.py
+++ unpacker.py
@@ -1,5 +1,16 @@
-import os, sys
+import os, sys, subprocess
 
-print("Location of 7za.exe")
-print(sys._MEIPASS)
-x = input("Press any key to continue")
+#print("Location of 7za.exe")
+#print(sys._MEIPASS)
+#x = input("Press any key to continue")
+zdir = os.path.join(sys._MEIPASS, "7za.exe")
+
+name_of_zipfile = "ALOVME2.7z"
+
+#number of files, 0 indexed
+for n in range(1):
+    f = name_of_zipfile + "." + str(n)
+    if not os.path.isfile(f):
+        raise Exception(f + " is missing! Please download all the necessary files!")
+    
+check = subprocess.run([zdir, "x", '-o"ALOV"', name_of_zipfile + ".0"], check=True, shell=True)
