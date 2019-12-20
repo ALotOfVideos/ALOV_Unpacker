8ba957593edddecd1d78e79d003175b07fac8559
AlphaAtlas <46462706+AlphaAtlas@users.noreply.github.com>
2019-12-20 12:13:27 -0500

    use batch file instead

--- buildexe.bat
+++ buildexe.bat
@@ -1,6 +1,6 @@
 @REM Makes a ALOV Unpacker .exe!
 python.exe -m pip install pyinstaller --upgrade
-pyinstaller unpacker.py --add-data "7za.exe;7z" -F 
+pyinstaller unpacker.py --add-data "7za.exe;7z" --add=data "A_Lot_Of_Videos_ALOV_for_ME3_-_Unpacker_for_1.1.1.bat;7z" -F 
 RD /S /Q __pycache__
 RD /S /Q build
 DEL /Q unpacker.spec
--- unpacker.py
+++ unpacker.py
@@ -1,16 +1,39 @@
-import os, sys, subprocess
+import os, sys, shutil, subprocess, glob
 
 #print("Location of 7za.exe")
 #print(sys._MEIPASS)
 #x = input("Press any key to continue")
-zdir = os.path.join(sys._MEIPASS, "7za.exe")
+#zdir = os.path.join(sys._MEIPASS, "7za.exe")
 
-name_of_zipfile = "ALOVME2.7z"
+#Save location of 7zip
+zdir = os.path.join(sys._MEIPASS, "7z", "7z.exe")
+
+#Save location of the first .bat file (with any name) saved in the exe
+d = glob.glob(os.path.join(sys._MEIPASS, "7z", "**/*.bat"), recursive = True)
+batdir = d[0]
+
+#specify destination directories
+dzdir = os.path.join(os.getcwd(), os.path.basename(batdir))
+dbatdir = os.path.join(os.getcwd(), os.path.basename(zdir))
+
+#Copy 7z and the bat to the .exe's location
+shutil.copy(zdir, dzdir)
+shutil.copy(batdir, dbatdir)
+#The location of the batch script
+
+#run the bat file
+subprocess.run(['"' + dbatdir + '"'], check=True, shell=True)
+
+#clean up
+os.remove(dzdir)
+os.remove(dbatdir)
+
+#name_of_zipfile = "ALOVME2.7z"
 
 #number of files, 0 indexed
-for n in range(1):
-    f = name_of_zipfile + "." + str(n)
-    if not os.path.isfile(f):
-        raise Exception(f + " is missing! Please download all the necessary files!")
+#for n in range(1):
+#    f = name_of_zipfile + "." + str(n)
+#    if not os.path.isfile(f):
+#        raise Exception(f + " is missing! Please download all the necessary files!")
     
-check = subprocess.run([zdir, "x", '-o"ALOV"', name_of_zipfile + ".0"], check=True, shell=True)
+#check = subprocess.run([zdir, "x", '-o"ALOV"', name_of_zipfile + ".0"], check=True, shell=True)
